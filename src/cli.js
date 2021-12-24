4339eb057b25ab79200645e7e70c61f22e2f01ff
dianalaput <dianaroman@mail.com>
2021-12-24 13:33:35 +0300

    +readlineSync

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,4 @@
-//#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
 const name = readlineSync.question('Your answer: ');
 console.log("Hello, " + name + "!");
